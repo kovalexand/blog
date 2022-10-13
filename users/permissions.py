8791ca0cbcfb1397f6b1cf78abb9a7aed0535f28
Alexander <kovalexander13@gmail.com>
2022-10-13 03:02:03 +0300

    Current user and owner permissions create

---
+++ users/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+
+class IsCurrentUserAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(obj and request.user and request.user.pk == obj.pk)
+
+
+class IsCurrentOwnerAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(obj and request.user and request.user.pk == obj.owner.pk)
