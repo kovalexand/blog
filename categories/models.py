bf27356b0816f37be9008a84f48ad630ffec609e
Alexander <kovalexander13@gmail.com>
2022-10-13 16:30:27 +0300

    create Category model

---
+++ categories/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+UserModel = get_user_model()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    about = models.CharField(max_length=100, blank=True)
+    owner = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.title
