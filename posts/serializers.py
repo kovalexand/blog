b48068fd6b538818bfa4237edbed14c7ce4de1af
Alexander <kovalexander13@gmail.com>
2022-10-15 03:30:50 +0300

    add comments to Post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
+from comments.serializers import CommentSerializer
 from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(read_only=True)
     category = serializers.PrimaryKeyRelatedField(read_only=True)
+    comments = CommentSerializer(write_only=True)
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'owner', 'category', 'created_at')
+        fields = ('id', 'title', 'comments', 'content', 'owner', 'category', 'created_at')
