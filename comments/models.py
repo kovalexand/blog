2e33ed3ea314d2db37d2d0890e7ecd14fc5b28d3
Alexander <kovalexander13@gmail.com>
2022-10-17 03:14:08 +0300

    delete __str__ method

--- comments/models.py
+++ comments/models.py
@@ -10,12 +10,8 @@ class Comment(models.Model):
     content = models.CharField(max_length=100)
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-created_at', )
-    
-    def __str__(self):
-        return self.author.username
