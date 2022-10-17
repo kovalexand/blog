afb80d293ef2cbcde69837cc64d04a8d4622521f
Alexander <kovalexander13@gmail.com>
2022-10-17 03:15:35 +0300

    change fields and add post serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 
 from categories.models import Category
+from posts.serializers import PostSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
-        fields = ('id', 'title', 'about', 'owner', 'created_at')
+        fields = ('id', 'title', 'posts', 'about', 'owner', 'created_at')
