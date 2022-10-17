4ba853bc0fe05a033ccc3553a63e5487e7e33105
Alexander <kovalexander13@gmail.com>
2022-10-17 03:16:10 +0300

    add SerializerMethodField()

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = SerializerMethodField()
     post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'owner', 'post', 'created_at', 'updated_at']
+        fields = ['id', 'content', 'author', 'post', 'created_at', 'updated_at']
+
+    @staticmethod
+    def get_author(obj):
+        return obj.author.username
