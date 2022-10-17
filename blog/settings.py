d116e7716e6e9c95838f45be2f530ff307b789cc
Alexander <kovalexander13@gmail.com>
2022-10-17 03:17:00 +0300

    add secret key to env

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4+q72%g86=5w0s8b9zj%$i5p&l)1-!8r@cuv7+!w@u%zv9(56'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
