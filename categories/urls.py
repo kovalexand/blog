fcfa2b83ab71521f9e895d0c0b7c72b45c3ec457
Alexander <kovalexander13@gmail.com>
2022-10-21 06:16:38 +0300

    delete urls from apps

--- categories/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from categories import views
-
-app_name = 'categories'
-router = DefaultRouter()
-router.register(r'categories', views.CategoryViewSet, basename="category")
-router.register(r'users/(?P<user_id>[^/.]+)/categories', views.UserCategoryViewSet, basename="user-category")
-
-urlpatterns = router.urls
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from users import views
-
-app_name = 'users'
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet, basename="user")
-
-urlpatterns = router.urls
