0d128bf7c3867a3e03bff3797dca01cf15c20db2
Alexander <kovalexander13@gmail.com>
2022-10-21 06:14:46 +0300

    add new permission

--- users/permissions.py
+++ users/permissions.py
@@ -1,11 +1,25 @@
 from rest_framework import permissions
 
 
-class IsCurrentUserAuthenticated(permissions.BasePermission):
+class IsCurrentUserAuthenticatedOrReadOnly(permissions.BasePermission):
     lookup_url_kwarg = 'user_id'
 
     def has_permission(self, request, view):
         return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_authenticated
+            and self.lookup_url_kwarg in view.kwargs
+            and int(view.kwargs[self.lookup_url_kwarg]) == request.user.pk
+        )
+
+
+class IsCurrentUserAuthenticatedOrReadAndCreateOnly(permissions.BasePermission):
+    lookup_url_kwarg = 'user_id'
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.method == 'POST' or
             request.user and request.user.is_authenticated
             and self.lookup_url_kwarg in view.kwargs
             and int(view.kwargs[self.lookup_url_kwarg]) == request.user.pk
