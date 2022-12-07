14c76c40151ea295ac0aab692b2d9cc62716718b
1terator <kovalexander13@gmail.com>
2022-12-07 08:32:31 +0300

    update cors

--- blog/settings.py
+++ blog/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
     'django_filters',
     'users',
     'categories',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -149,5 +151,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:1337",
+]
 
 LOGIN_URL = 'login'
